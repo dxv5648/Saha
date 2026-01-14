90d954caeef54bdd62fc027172de304e7cad961d
dxv5648 <dxv5648@autuni.ac.nz>
2026-01-14 16:13:16 +1300

    Feature Can post to a database

--- saha-project/src/home-page/home.jsx
+++ saha-project/src/home-page/home.jsx
@@ -7,6 +7,7 @@ import AI from "./ai-search.jsx";
 import ServiceOverview from "./service-overview.jsx";
 import Contact from "./contact.jsx";
 import Background from "./background.jsx";
+import ServicePost from "../service-page/service-post.jsx";
 
 export default function Home() {
   const location = useLocation();
@@ -26,6 +27,7 @@ export default function Home() {
       <Header />
       <Body />
       <Footer className="mt-auto" />
+      <ServicePost />
     </div>
   );
 }
