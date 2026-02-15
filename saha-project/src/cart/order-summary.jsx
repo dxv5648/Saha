25233a28eba1fc7591ad707994a4094269996008
gargmaalav <gargmaalav@gmail.com>
2026-02-15 18:38:01 +1300

    Update order-summary.jsx

--- saha-project/src/cart/order-summary.jsx
+++ saha-project/src/cart/order-summary.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../auth/useAuth.js";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../auth/AuthProvider";
 import supabase from "../supabase-client";
 
 export default function OrderSummary() {
